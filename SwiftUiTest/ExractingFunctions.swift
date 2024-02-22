27d2bab18f080c2be007313a76da50743751bdef
Dileep <mdilleeep09@gmail.com>
2024-02-22 14:08:01 +0530

    webView

---
+++ SwiftUiTest/ExractingFunctions.swift
@@ -0,0 +1,18 @@
+//
+//  ExractingFunctions.swift
+//  SwiftUiTest
+//
+//  Created by Cv infotech on 22/02/24.
+//
+
+import SwiftUI
+
+struct ExractingFunctions: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExractingFunctions()
+}
---
+++ SwiftUiTest/WebView.swift
@@ -0,0 +1,18 @@
+//
+//  WebView.swift
+//  SwiftUiTest
+//
+//  Created by Cv infotech on 22/02/24.
+//
+
+import SwiftUI
+
+struct WebView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WebView()
+}
