d6541c3550de4d1dc6db52ad590f59499a5dfc54
Dileep <mdilleeep09@gmail.com>
2024-02-22 18:07:09 +0530

    22 last push

---
+++ SwiftUiTest/Animations.swift
@@ -0,0 +1,18 @@
+//
+//  Animations.swift
+//  SwiftUiTest
+//
+//  Created by Cv infotech on 22/02/24.
+//
+
+import SwiftUI
+
+struct Animations: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Animations()
+}
---
+++ SwiftUiTest/Ifelse.swift
@@ -0,0 +1,18 @@
+//
+//  Ifelse.swift
+//  SwiftUiTest
+//
+//  Created by Cv infotech on 22/02/24.
+//
+
+import SwiftUI
+
+struct Ifelse: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Ifelse()
+}
