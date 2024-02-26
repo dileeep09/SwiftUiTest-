3a39a7efe734535ca2b4cc0ed6f0f1997f5b653d
Dileep <mdilleeep09@gmail.com>
2024-02-26 11:27:01 +0530

    26/02

---
+++ SwiftUiTest/List.swift
@@ -0,0 +1,18 @@
+//
+//  List.swift
+//  SwiftUiTest
+//
+//  Created by Cv infotech on 26/02/24.
+//
+
+import SwiftUI
+
+struct List: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    List()
+}
